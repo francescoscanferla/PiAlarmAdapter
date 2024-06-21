182a545de7c178a03ebd8aad78551c5aca3f92d4
Francesco Scanferla <info@francescoscanferla.it>
2024-06-21 08:27:19 +0200

    created SensorsService

--- src/container.py
+++ src/container.py
@@ -2,7 +2,7 @@ from dependency_injector import containers, providers
 
 from src.config import MqttConfig, SensorsConfig
 from src.mqtt_client import MqttClient
-from src.services import MqttService
+from src.services import MqttService, SensorsService
 
 
 class AppContainer(containers.DeclarativeContainer):
@@ -10,3 +10,4 @@ class AppContainer(containers.DeclarativeContainer):
     sensors_config = providers.Singleton(SensorsConfig.load_from_env)
     mqtt_client = providers.Singleton(MqttClient, config=mqtt_config)
     mqtt_service = providers.Singleton(MqttService, mqtt_client)
+    sensors_service = providers.Singleton(SensorsService, sensors_config)
--- src/services.py
+++ src/services.py
@@ -1,3 +1,8 @@
+from typing import Dict
+
+from gpiozero import Button
+
+from src.config import SensorsConfig
 from src.mqtt_client import MqttClient
 
 
@@ -13,5 +18,30 @@ class MqttService:
     def publish_message(self, topic, message) -> None:
         self.mqtt_client.publish_message(self._get_topic(topic), message)
 
+    def connect(self):
+        self.mqtt_client.connect()
+
     def disconnect(self) -> None:
         self.mqtt_client.disconnect()
+
+
+class SensorsService:
+    sensors: Dict[int, Button] = {}
+
+    def __init__(self, sensors_config: SensorsConfig):
+        self.config = sensors_config
+
+    @classmethod
+    def on_close(cls, btn):
+        print("Close: ", btn)
+
+    @classmethod
+    def on_open(cls, btn):
+        print("Open: ", btn)
+
+    def init_sensors(self):
+        for k, v in self.config.sensors.items():
+            button = Button(k)
+            button.when_activated = SensorsService.on_close
+            button.when_deactivated = SensorsService.on_open
+            self.sensors[k] = button
