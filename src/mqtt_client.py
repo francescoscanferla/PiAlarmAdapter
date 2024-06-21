51adfb20b5c36632a36da7d2020f8f91a0a1dd73
Francesco Scanferla <info@francescoscanferla.it>
2024-06-21 08:25:30 +0200

    moved connect into method

--- src/mqtt_client.py
+++ src/mqtt_client.py
@@ -11,17 +11,19 @@ class MqttClient:
         self.client = mqtt.Client()
         self.client.on_connect = self._on_connect
         self.client.username_pw_set(config.username, config.password)
-        self.logger.info("Attempting to connect to %s:%s", config.broker_url, config.broker_port)
-        self.client.connect(config.broker_url, config.broker_port)
 
     def _on_connect(self, client, userdata, flags, reason_code, properties):
         self.logger.info("Connected to broker: %s:%s with code %s",
                          self.config.broker_url, self.config.broker_port, reason_code)
 
-    def publish_message(self, topic, message) -> None:
-        self.client.publish(topic, message)
-        self.logger.info("Sent message: %s to topic: %s", message, topic)
+    def connect(self):
+        self.logger.info("Attempting to connect to %s:%s", self.config.broker_url, self.config.broker_port)
+        self.client.connect(self.config.broker_url, self.config.broker_port)
 
     def disconnect(self) -> None:
         self.logger.info("Disconnecting from %s:%s", self.config.broker_url, self.config.broker_port)
         self.client.disconnect()
+
+    def publish_message(self, topic, message) -> None:
+        self.client.publish(topic, message)
+        self.logger.info("Sent message: %s to topic: %s", message, topic)
